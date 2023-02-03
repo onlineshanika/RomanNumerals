268824bbccb014cc90c5a8219481176735b08dd6
Shanika Jajyasekara <online.shanika@gmail.com>
2023-02-03 12:39:44 +0000

    new testcase - with multiple inputs

--- src/test/java/com/techreturners/numbers/RomanNumeralsTest.java
+++ src/test/java/com/techreturners/numbers/RomanNumeralsTest.java
@@ -2,6 +2,8 @@ package com.techreturners.numbers;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -15,6 +17,16 @@ public class RomanNumeralsTest {
         assertEquals("MMCMXLVIII", romanNumerals.convertToRomanNumerals(2948));
 
     }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/testData.csv")
+    public void testWithCsvFileSourceFromClasspath(int arabic, String roman) throws OutOfRangeException {
+
+        RomanNumerals romanNumerals = new RomanNumerals();
+        assertEquals(roman, romanNumerals.convertToRomanNumerals(arabic));
+
+    }
+
 
     @Test
     public void numberOutOfGivenRangeTest() {
